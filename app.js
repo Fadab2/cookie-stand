8bf18306db713c43592a13dffe850df5137f04aa
Edris Berg <ali_berg1@yahoo.com>
2021-09-20 16:57:56 -0700

    added index.html

--- app.js
+++ app.js
@@ -1,39 +1,59 @@
+let parentEl = document.getElementById('list');
+
 let Seattle = {
-    minHours: 23,
-    maxHours: 65,
-    custPerHr: function () {
-        Math.floor(Math.random() * 10)
-    }
-}
+    minCustPerHr: 23,
+    maxCustPerHr: 65,
+    avgCookiePerCus: 44,
+    results: [],
+    numOfCusPerHr: function (minCustPerHr, maxCustPerHr) {
+        let custPerHr = Math.floor(Math.random() * (this.maxCustPerHr - this.minCustPerHr + 1) + this.minCustPerHr);
+        console.log(custPerHr);
 
-let tokyo = {
-    minHours: 3,
-    maxHours: 24,
-    custPerHr: function () {
-        Math.floor(Math.random() * 10)
-    }
-}
+        let listEl = document.createElement('ul')
 
-let dubai = {
-    minHours: 11,
-    maxHours: 38,
-    custPerHr: function () {
-        Math.floor(Math.random() * 10)
-    }
-}
+        for (let i = 0; i < this.results.length; i++) {
+            let amountOfcookies = this.avgCookiePerCus * custPerHr;
 
-let paris = {
-    minHours: 20,
-    maxHours: 38,
-    custPerHr: function () {
-        Math.floor(Math.random() * 10)
-    }
-}
+            let hourlyEl = document.createElement('li')
+            hourlyEl.innerText = this.results[i];
 
-let lima = {
-    minHours: 2,
-    maxHours: 16,
-    custPerHr: function () {
-        Math.floor(Math.random() * 10)
+            listEl.appendChild(hourlyEl);
+
+        }
+        parentEl.appendChild(listEl);
     }
-}
+
+};
+
+
+// let tokyo = {
+//     minHours: 3,
+//     maxHours: 24,
+//     avrgeCookiesPerCus: function () {
+//         Math.floor(Math.random() * 10)
+//     }
+// }
+
+// let dubai = {
+//     minHours: 11,
+//     maxHours: 38,
+//     avrgeCookiesPerCus: function () {
+//         Math.floor(Math.random() * 10)
+//     }
+// }
+
+// let paris = {
+//     minHours: 20,
+//     maxHours: 38,
+//     cavrgeCookiesPerCus: function () {
+//         Math.floor(Math.random() * 10)
+//     }
+// }
+
+// let lima = {
+//     minHours: 2,
+//     maxHours: 16,
+//     avrgeCookiesPerCus: function () {
+//         Math.floor(Math.random() * 10)
+//     }
+// }
