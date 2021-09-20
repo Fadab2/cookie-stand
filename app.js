c5dfac81c75647b170f9696b70bc99bcce7c9af2
Edris Berg <ali_berg1@yahoo.com>
2021-09-20 14:56:15 -0700

    added sales.html, app.js

---
+++ app.js
@@ -0,0 +1,39 @@
+let Seattle = {
+    minHours: 23,
+    maxHours: 65,
+    custPerHr: function () {
+        Math.floor(Math.random() * 10)
+    }
+}
+
+let tokyo = {
+    minHours: 3,
+    maxHours: 24,
+    custPerHr: function () {
+        Math.floor(Math.random() * 10)
+    }
+}
+
+let dubai = {
+    minHours: 11,
+    maxHours: 38,
+    custPerHr: function () {
+        Math.floor(Math.random() * 10)
+    }
+}
+
+let paris = {
+    minHours: 20,
+    maxHours: 38,
+    custPerHr: function () {
+        Math.floor(Math.random() * 10)
+    }
+}
+
+let lima = {
+    minHours: 2,
+    maxHours: 16,
+    custPerHr: function () {
+        Math.floor(Math.random() * 10)
+    }
+}
